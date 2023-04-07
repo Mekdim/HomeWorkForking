a32c84e907b4914eafcf18a550772a3b04c5a5a7
Johnson Odejide <121649614+johnsondatadev@users.noreply.github.com>
2023-04-07 22:16:24 +0100

    Update Forking Homework

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,11 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+# Data Table
+write_csv(diamonds, 'diamonds.csv')
+diamonds <- fread('diamonds.csv')
+# diamonds[, .(carat, cut, price)]
+ndiamond <- diamonds[(price > 7000 & price < 10000 & cut == "Premium") , .(carat, cut, price)][order(-carat)]
+head(ndiamond, 20)
+# ndiamond[1:20, ]
