d3e268df0dd7826da862b0f2c447843810559503
Johnson Odejide <121649614+johnsondatadev@users.noreply.github.com>
2023-04-07 07:07:17 +0100

    Update ForkingHomeworkRFile
    Added code for data.table.

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,6 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+ndiamond <- diamonds[cut == "Premium", (price > 7000 & price < 10000) , .(carat, cut, price)][order(-carat)]
+head(ndiamond, 20)
